a6147dbee88bcc3a2bfa270ba5a618ce5ffa4dd9
Ishan Kafle <www.2020ishankafle@gmail.com>
2023-06-29 10:19:43 +0545

    'testing-chapter3'

--- helloworld_project/pages/urls.py
+++ helloworld_project/pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import homePageView
+from .views import AboutPageView, HomePageView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),  # new
 ]
