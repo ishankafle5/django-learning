db76fcc96cc7bb75ebb4ac7785ac2f8c94651283
Ishan Kafle <www.2020ishankafle@gmail.com>
2023-06-29 11:51:12 +0545

    New updates for Heroku deployment

--- helloworld_project/helloworld_project/settings.py
+++ helloworld_project/helloworld_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@4j2!$iuj^q4e+e+2jh*hp2505=mm3zt&n_kl!yrz!r^wp*ow!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
